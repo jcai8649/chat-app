13738a771330d71be0fff88a0767c0764746cc56
Jackson Cai <jcai8649@gmail.com>
2020-11-29 06:36:46 -0800

    Add profanity filter

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,57 @@
 const socket = io()
 
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+const $locationButton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-socket.on('sendMessage', (message) => {
-    console.log(message)
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    //disable
+    $messageFormButton.setAttribute('disabled','disabled')
+
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+
+        //enable
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+        
+        console.log('The message was delivered!')
+    })
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
-    e.preventDefault()
-    const message = e.target.elements.message.value
-    socket.emit('sendMessage', message)
+$locationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolcation is not supported by your browser.')
+    }
+
+    //disable
+    $locationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        
+        socket.emit('sendLocation', {
+            lat: position.coords.latitude, 
+            long: position.coords.longitude
+        }, () => {
+            //enable
+            $locationButton.removeAttribute('disabled')
+            console.log('Location Shared')
+        })
+    })
 })
