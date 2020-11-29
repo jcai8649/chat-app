3856300f7371e3faab53d26b5d422f349c4484bb
Jackson Cai <jcai8649@gmail.com>
2020-11-29 01:56:08 -0800

    Create socket.io operations

---
+++ public/js/chat.js
@@ -0,0 +1,15 @@
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+socket.on('sendMessage', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
+})
